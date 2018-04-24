f02327f4e0445c371a8d5189233b82fd472f17b5
vvinay75 <31302099+vvinay75@users.noreply.github.com>
2018-04-24 03:03:28 -0500

    DL LAB2
    deepl learning lab2

---
+++ DL_LABASSIGN-2/Source/Evaluate.py
@@ -0,0 +1,97 @@
+#! /usr/bin/env python
+
+import csv
+import os
+
+import numpy as np
+import tensorflow as tf
+from tensorflow.contrib import learn
+
+import DataHelper
+
+# Parameters
+# ==================================================
+
+# Data Parameters
+tf.flags.DEFINE_string("alt_data", "./newsgroups/test/alt", "Data source for articles about atheism.")
+tf.flags.DEFINE_string("comp_data", "./newsgroups/test/comp", "Data source for computer articles.")
+tf.flags.DEFINE_string("misc_data", "./newsgroups/test/misc", "Data source for sales ads.")
+tf.flags.DEFINE_string("rec_data", "./newsgroups/test/rec", "Data source for sports articles.")
+tf.flags.DEFINE_string("sci_data", "./newsgroups/test/sci", "Data source for scientific articles.")
+tf.flags.DEFINE_string("soc_data", "./newsgroups/test/soc", "Data source for religious articles.")
+tf.flags.DEFINE_string("talk_data", "./newsgroups/test/talk", "Data source for open forums.")
+
+# Eval Parameters
+tf.flags.DEFINE_integer("batch_size", 64, "Batch Size (default: 64)")
+tf.flags.DEFINE_string("checkpoint_dir", "./runs/1511652854/checkpoints", "Checkpoint directory from training run")
+tf.flags.DEFINE_boolean("eval_train", True, "Evaluate on all training data")
+
+# Misc Parameters
+tf.flags.DEFINE_boolean("allow_soft_placement", True, "Allow device soft device placement")
+tf.flags.DEFINE_boolean("log_device_placement", False, "Log placement of ops on devices")
+
+
+FLAGS = tf.flags.FLAGS
+FLAGS._parse_flags()
+print("\nParameters:")
+for attr, value in sorted(FLAGS.__flags.items()):
+    print("{}={}".format(attr.upper(), value))
+print("")
+
+data_flags = [FLAGS.alt_data, FLAGS.comp_data, FLAGS.misc_data, FLAGS.rec_data,
+              FLAGS.sci_data, FLAGS.soc_data, FLAGS.talk_data]
+if FLAGS.eval_train:
+    x_raw, y_test = DataHelper.load_data_and_labels(data_flags)
+    y_test = np.argmax(y_test, axis=1)
+
+# Map data into vocabulary
+vocab_path = os.path.join(FLAGS.checkpoint_dir, "..", "vocab")
+vocab_processor = learn.preprocessing.VocabularyProcessor.restore(vocab_path)
+x_test = np.array(list(vocab_processor.transform(x_raw)))
+
+print("\nEvaluating...\n")
+
+# Evaluation
+# ==================================================
+checkpoint_file = tf.train.latest_checkpoint(FLAGS.checkpoint_dir)
+graph = tf.Graph()
+with graph.as_default():
+    session_conf = tf.ConfigProto(
+      allow_soft_placement=FLAGS.allow_soft_placement,
+      log_device_placement=FLAGS.log_device_placement)
+    sess = tf.Session(config=session_conf)
+    with sess.as_default():
+        # Load the saved meta graph and restore variables
+        saver = tf.train.import_meta_graph("{}.meta".format(checkpoint_file))
+        saver.restore(sess, checkpoint_file)
+
+        # Get the placeholders from the graph by name
+        input_x = graph.get_operation_by_name("input_x").outputs[0]
+        # input_y = graph.get_operation_by_name("input_y").outputs[0]
+        dropout_keep_prob = graph.get_operation_by_name("dropout_keep_prob").outputs[0]
+
+        # Tensors we want to evaluate
+        predictions = graph.get_operation_by_name("output/predictions").outputs[0]
+
+        # Generate batches for one epoch
+        batches = DataHelper.batch_iter(list(x_test), FLAGS.batch_size, 1, shuffle=False)
+
+        # Collect the predictions here
+        all_predictions = []
+
+        for x_test_batch in batches:
+            batch_predictions = sess.run(predictions, {input_x: x_test_batch, dropout_keep_prob: 1.0})
+            all_predictions = np.concatenate([all_predictions, batch_predictions])
+
+# Print accuracy if y_test is defined
+if y_test is not None:
+    correct_predictions = float(sum(all_predictions == y_test))
+    print("Total number of test examples: {}".format(len(y_test)))
+    print("Accuracy: {:g}".format(correct_predictions/float(len(y_test))))
+
+# Save the evaluation to a csv
+predictions_human_readable = np.column_stack((np.array(x_raw), all_predictions))
+out_path = os.path.join(FLAGS.checkpoint_dir, "..", "prediction.csv")
+print("Saving evaluation to {0}".format(out_path))
+with open(out_path, 'w') as f:
+    csv.writer(f).writerows(predictions_human_readable)
